51c2d2ebdb018bb1d3a57079369cb8fb22766928
Hudson, Philip <Philip.Hudson@zuhlke.com>
2019-01-11 12:22:34 +0000

    (feat): add spring module

---
+++ records-microservice/src/main/java/com/philhudson/recordsspringmongo/RecordsSpringMongoApplication.java
@@ -0,0 +1,12 @@
+package com.philhudson.recordsspringmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordsSpringMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordsSpringMongoApplication.class, args);
+	}
+}
